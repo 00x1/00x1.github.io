c7ecb1c60196b5e52b8a2a37d15fb5ac61a4dc56
Jarett S <dj_2cb@yahoo.com>
2024-03-09 20:23:15 -0500

    Updated timer.js
    Just adjusted the countdown

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Mar 30, 2024 00:00:00").getTime();
+    const countDate = new Date("Dec 31, 2024 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     const gap = countDate - currentTime;
